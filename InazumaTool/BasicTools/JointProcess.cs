c200dd6666acc7c3ecf159a70c5a3c7722d5c34b
yO-oygdrasil <wyaself@126.com>
2018-09-17 02:38:30 +0800

    a

--- InazumaTool/BasicTools/JointProcess.cs
+++ InazumaTool/BasicTools/JointProcess.cs
@@ -327,7 +327,31 @@ namespace InazumaTool.BasicTools
             {
                 ClearHierachyJointsRotation();
             }));
-            cmdList.Add(new CommandData("骨骼", cmdStr, "beautifulPole", "为漂亮的极向量铺路", () =>
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_x", "极向量清除X", () =>
+            {
+                BeautifulPole(null, true, false, false);
+            }));
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_y", "极向量清除Y", () =>
+            {
+                BeautifulPole(null, false, true, false);
+            }));
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_z", "极向量清除Z", () =>
+            {
+                BeautifulPole(null, false, false, true);
+            }));
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_y", "极向量清除XY", () =>
+            {
+                BeautifulPole(null, true, true, false);
+            }));
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_y", "极向量清除XZ", () =>
+            {
+                BeautifulPole(null, true, false);
+            }));
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_y", "极向量清除YZ", () =>
+            {
+                BeautifulPole(null, false);
+            }));
+            cmdList.Add(new CommandData("骨骼/优雅极向量", cmdStr, "beautifulPole_y", "极向量清除XYZ", () =>
             {
                 BeautifulPole(null);
             }));
